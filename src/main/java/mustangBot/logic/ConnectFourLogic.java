9cb64d72b613960411b4a3ffc40ea7dbb5af52ad
Austin W <awalte4@clemson.edu>
2019-11-30 19:34:12 -0500

    Started working on ConnectFourLogic

--- src/main/java/mustangBot/logic/ConnectFourLogic.java
+++ src/main/java/mustangBot/logic/ConnectFourLogic.java
@@ -1,4 +1,15 @@
 package mustangBot.logic;
 
 public class ConnectFourLogic {
+
+    private static int ROWS;
+    private static int COLUMNS;
+
+    public ConnectFourLogic(int r, int c){
+        ROWS = r;
+        COLUMNS = c;
+    }
+
+
+
 }
