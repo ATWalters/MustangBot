8020e9824d5a8c23ab28805e5ba267faae9e6dce
Austin W <awalte4@clemson.edu>
2020-02-15 00:06:47 -0500

    Got MagicEightBallCommand working completely. Added a file containing the possible magic 8 ball answers.

--- src/main/java/mustangBot/commands/ConnectFourCommand.java
+++ src/main/java/mustangBot/commands/ConnectFourCommand.java
@@ -3,11 +3,14 @@ package mustangBot.commands;
 import com.jagrosh.jdautilities.command.*;
 import com.jagrosh.jdautilities.commons.waiter.*;
 
+import java.io.*;
+import java.util.*;
+
 public class ConnectFourCommand extends Command {
 
     private EventWaiter waiter;
 
-    public ConnectFourCommand(EventWaiter w) {
+    public ConnectFourCommand(EventWaiter w) throws IOException {
         this.name = "c4";
         this.help = "Starts a Connect Four game with another server member";
         this.aliases = new String[] {"cf"};
--- src/main/java/mustangBot/commands/MagicEightBallCommand.java
+++ src/main/java/mustangBot/commands/MagicEightBallCommand.java
@@ -1,18 +1,47 @@
 package mustangBot.commands;
 
 import com.jagrosh.jdautilities.command.*;
+import net.dv8tion.jda.api.entities.*;
+
+import java.io.*;
+import java.util.*;
 
 public class MagicEightBallCommand extends Command {
 
-    public MagicEightBallCommand(){
+    private ArrayList<String> answers = new ArrayList<String>();
+    private Member initiator;
+
+    public MagicEightBallCommand() throws IOException {
         this.name = "8ball";
         this.aliases = new String[] {"magic8ball"};
         this.help = "A command to ask a Magic 8 Ball a question";
         this.arguments = "[Question to ask]";
+        this.fillAnswers();
     }
 
     @Override
     protected void execute(CommandEvent event){
+        String[] messageSent = event.getArgs().split(" ");
+        initiator = event.getMember();
 
+        if(messageSent.length < 2){
+            event.reply(initiator.getAsMention() + ", I'm sorry but you did not ask a question.");
+        }else{
+            int index = (int)(Math.random()*((this.answers.size())));
+            event.reply(this.answers.get(index));
+        }
+    }
+
+    private void fillAnswers() throws IOException {
+        File file = new File("D:\\CS Side Projects\\MustangBot\\8ball answers.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        int i = 0;
+        while ((st = br.readLine()) != null) {
+
+            this.answers.add(i,st);
+            i++;
+        }
     }
 }
