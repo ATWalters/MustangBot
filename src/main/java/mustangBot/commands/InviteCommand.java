21aaca904d8310f8ccca35a49fa58d595ece6cd9
Austin W <awalte4@clemson.edu>
2020-02-15 00:35:52 -0500

    InviteCommand is working. May add more functionality later not sure.

--- src/main/java/mustangBot/commands/InviteCommand.java
+++ src/main/java/mustangBot/commands/InviteCommand.java
@@ -2,8 +2,12 @@ package mustangBot.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.*;
 
 public class InviteCommand extends Command {
+
+    private Member initiator;
 
     public InviteCommand(){
         this.name = "invite";
@@ -14,6 +18,15 @@ public class InviteCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-        
+        String[] messageSent = event.getMessage().getContentRaw().split("");
+        initiator = event.getMember();
+
+        if(initiator.hasPermission(Permission.CREATE_INSTANT_INVITE)) {
+            if (messageSent.length == 1) {
+                event.reply("To invite someone give them this link: " + event.getTextChannel().createInvite().complete().getUrl());
+            }
+        }else{
+            event.reply("You do not have permission to invite people, ask someone who does to do this");
+        }
     }
 }
